49b378dae29a56ad7ec3eebc0bc64b0af333caea
Thomas Chaplin <tomrchaplin@gmail.com>
2023-12-22 12:17:26 +0000

    Add all option to CLI

--- src/common/io.rs
+++ src/common/io.rs
@@ -11,7 +11,22 @@ pub struct RunCode {
     problem: u32,
     run_a: bool,
     run_b: bool,
+    solution_a: Option<String>,
+    solution_b: Option<String>,
     as_example: bool,
+}
+
+impl RunCode {
+    pub fn init_all(problem: u32) -> Self {
+        Self {
+            problem,
+            run_a: true,
+            run_b: true,
+            solution_a: None,
+            solution_b: None,
+            as_example: false,
+        }
+    }
 }
 
 impl FromStr for RunCode {
@@ -31,12 +46,14 @@ impl FromStr for RunCode {
             problem,
             run_a,
             run_b,
+            solution_a: None,
+            solution_b: None,
             as_example,
         })
     }
 }
 
-fn print_solution(prefix: &str, solution: Option<String>) {
+fn print_solution(prefix: &str, solution: &Option<String>) {
     let solution_str = match solution {
         Some(s) => s.to_string().bold().cyan(),
         None => "TODO".to_string().dimmed(),
@@ -73,17 +90,24 @@ impl RunCode {
         fs::read_to_string(&path).map_err(|_e| AocRunError::NoFile(path))
     }
 
-    pub fn run(&self) -> Result<(), AocRunError> {
+    pub fn run(mut self) -> Result<Self, AocRunError> {
         let problem = days::get_problem(self.problem)?;
         let input = self.get_input()?;
         if self.run_a {
-            let solution_a = problem.solve_a(&input);
-            print_solution("A", solution_a);
-        };
+            self.solution_a = problem.solve_a(&input);
+        }
         if self.run_b {
-            let solution_b = problem.solve_b(&input);
-            print_solution("B", solution_b);
-        };
-        Ok(())
+            self.solution_b = problem.solve_b(&input);
+        }
+        Ok(self)
+    }
+
+    pub fn print(&self) {
+        if self.run_a {
+            print_solution("A", &self.solution_a);
+        }
+        if self.run_b {
+            print_solution("B", &self.solution_b);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,25 @@ use std::{env, str::FromStr};
 
 pub fn main() {
     let args: Vec<_> = env::args().collect();
-    assert_eq!(args.len(), 2);
-    let run_code = RunCode::from_str(&args[1]).expect("Valid run code");
-    let res = run_code.run();
-    if let Err(e) = res {
-        println!("{}", e)
+    assert!(args.len() <= 2);
+
+    if args.len() == 1 || args[1] == "all" {
+        for i in 1.. {
+            let res = RunCode::init_all(i as u32).run();
+            match res {
+                Err(common::io::AocRunError::UnregistedProblem(_)) => break,
+                Ok(rc) => {
+                    println!("Day {}", i);
+                    rc.print()
+                }
+                Err(e) => println!("{}", e),
+            }
+        }
+    } else {
+        let res = RunCode::from_str(&args[1]).expect("Valid run code").run();
+        match res {
+            Ok(rc) => rc.print(),
+            Err(e) => println!("{}", e),
+        }
     }
 }
